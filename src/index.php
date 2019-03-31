b017aa5f8f3b6cec37206e7757e2469c9a0f25ce
Steven Pilatschek <steven.pilatschek@gmail.com>
2019-04-01 00:24:25 +0200

    Setup Routes and implemented Services-Page

--- src/index.php
+++ src/index.php
@@ -2,4 +2,12 @@
 require __DIR__.'/vendor/autoload.php';
 $loader = new Twig_Loader_Filesystem(__DIR__.'/templates');
 $twig = new Twig_Environment($loader);
-echo $twig->render('index.html');
+
+$actual_link = $_SERVER['REQUEST_URI'];
+
+if ("/services" == $actual_link) {
+    echo $twig->render('services.html');
+}
+else {
+    echo $twig->render('index.html');
+}
